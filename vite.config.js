3c45c005e39b688adc53ae6fdf9916b420ffb90b
Andy Gillon <andrew@modestudio.uk>
2023-08-10 11:20:42 +0100

    new build opts

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
-import glsl from 'vite-plugin-glsl'
+import glsl from "vite-plugin-glsl";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+const isCodeSandbox = "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+export default defineConfig({
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(`${__dirname}/src`, "index.html"),
+        s4: resolve(`${__dirname}/src`, "scenario04.html"),
+      },
     },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    },
-    plugins:
-    [
-        glsl()
-    ]
-}
+  },
+  plugins: [glsl()],
+});
